963d727cd8e02baf804c3a49fd0338ff8a450535
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2024-01-27 15:47:14 +0000

    added support for projectile attack, and players are now created on start

---
+++ Assets/ProjectileAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAttack : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform projectileSpawn;
+    public float fireRate;
+    float timer;
+    public float projectileForce;
+    public float damage;
+    public KeyCode attackKey;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer <= 0)
+        {
+            if (Input.GetKeyDown(attackKey))
+            {
+                GameObject proj = Instantiate(projectile, projectileSpawn.position, Quaternion.identity);
+                proj.GetComponent<Projectile>().damage = damage;
+                proj.GetComponent<Projectile>().shootingPlayer = gameObject;
+                proj.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileForce, ForceMode2D.Impulse);
+                timer = fireRate;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public GameObject player1Prefab;
+    public GameObject player2Prefab;
+    public GameObject blockTimer1;
+    public GameObject blockTimer2;
+
     public GameObject player1;
     public GameObject player2;
+
     public Vector2 player1StartPos;
     public Vector2 player2StartPos;
 
@@ -14,6 +22,12 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Instance = this;
+        player1 = Instantiate(player1Prefab, new Vector3(player1StartPos.x, player1StartPos.y,0), Quaternion.identity);
+        player2 = Instantiate(player2Prefab, new Vector3(player2StartPos.x,player2StartPos.y,0), Quaternion.identity);
+        blockTimer1.GetComponent<FollowPlayer>().objectToFollow = player1.transform.GetChild(3);
+        blockTimer2.GetComponent<FollowPlayer>().objectToFollow = player2.transform.GetChild(3);
+        player1.GetComponent<Block>().blockText = blockTimer1;
+        player2.GetComponent<Block>().blockText = blockTimer2;
     }
 
     // Update is called once per frame
