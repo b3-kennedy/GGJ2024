a519e99c60b341db43b25a87e9e46c1f93a6b3a1
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2024-01-28 14:00:01 +0000

    added character select

--- Assets/OnSceneLoad.cs
+++ Assets/OnSceneLoad.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class OnSceneLoad : MonoBehaviour
 {
-    public Transform objectToFollow;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.Instance.OnStartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(objectToFollow.position.x, objectToFollow.position.y, -5);
+        
     }
 }
